8c523cac56118264f4d46a0a35d49d30c164a899
Charles Kihe <charles@simplesessions.com>
2018-10-14 16:21:37 -1000

    Tons of config to make stylelint and postcss work; Started on nav

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
+    'postcss-mixins': {},
     'postcss-preset-env': {},
+    'postcss-nested': {},
+    'postcss-font-magician': {},
     cssnano: {},
     autoprefixer: {},
   },
