151e27d7fac04289c06637a0d75d1d0852c5f8f7
taylorkmho <taylorkmho@gmail.com>
2019-01-31 22:21:01 -0800

    updated map graphic, added markers

---
+++ scripts/components/VisitationMapMarker.vue
@@ -0,0 +1,18 @@
+<template>
+  <use :x="x" :y="y" xlink:href="#visitation-map__marker" />
+</template>
+
+<script>
+  export default {
+    name: 'VisitationMapMarker',
+    props: ['x', 'y'],
+    components: {},
+    mounted() {
+      console.log('😬 visitation map marker');
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+</style>
