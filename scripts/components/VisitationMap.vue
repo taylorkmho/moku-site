0e4f4a217a373f591b04cc696b446c31545db414
taylorkmho <taylorkmho@gmail.com>
2019-02-10 22:56:17 -0800

    placed and added new style to markers

--- scripts/components/VisitationMap.vue
+++ scripts/components/VisitationMap.vue
@@ -13,7 +13,8 @@
     name: 'VisitationMap',
     data() {
       return {
-
+        DEFAULT_ZOOM: 11.75,
+        DEFAULT_COORDS: [-155.040, 19.650]
       }
     },
     mounted() {
@@ -24,9 +25,9 @@
           center: [-160.500, 21.900],
           pitch: 0,
           zoom: 9,
-          minZoom: 6,
+          minZoom: 4,
           maxZoom: 18,
-          // interactive: false,
+          interactive: false,
         }
       );
 
@@ -44,7 +45,7 @@
           mapboxMap.addImage(name, image);
         })
       },
-      addPlace: function(options) {
+      addMarker: function(options) {
         const defaults = {
           id: 'default',
           icon: {
@@ -52,7 +53,6 @@
             size: 0.25,
           },
           coordinates: [-155.095, 19.715],
-          description: 'no desc',
           zoomRange: [0, 24]
         }
 
@@ -82,37 +82,58 @@
           "maxzoom": options.zoomRange[1]
         });
 
-        const popup = new mapboxgl.Popup({
-            offset: 50 * options.icon.size,
-            className: 'visitation-map-app__popup'
+        const isZoomer = !!options.zoomTo
+        if (isZoomer) {
+          this.mapboxMap.on('click', options.id, () => {
+            this.mapboxMap.easeTo({
+              zoom: options.zoomTo,
+              center: options.zoomToCoordinates || options.coordinates,
+            })
           })
-          .setHTML(`
-            <figure><img src='${options.logo}' /></figure>
-            <h2>${options.title}</h2>
-            <h3>${options.subtitle}</h3>
-            <p>${options.description}</p>
-          `)
-          .addTo(this.mapboxMap);
+        }
 
-        const popupClickEl = document.createElement('div');
-        popupClickEl.classList.add('visitation-map-app__marker');
+        const hasPopup = !!options.subtitle || !!options.description || !!options.logo;
+        let popup = null
+        if (hasPopup) {
+          let logo = '';
+          if (options.logo instanceof Array) {
+            options.logo.forEach((item)=>{
+              logo += `<figure><img src='${item}' /></figure>`
+            })
+          } else {
+            logo += `<figure><img src='${options.logo}' /></figure>`
+          }
 
-        const marker = new mapboxgl.Marker(popupClickEl)
+          popup = new mapboxgl.Popup({
+              offset: 50 * options.icon.size,
+              className: 'visitation-map-app__popup'
+            })
+            .setHTML(`
+              <header>${logo}</header>
+              <h2>${options.title}</h2>
+              <h3>${options.subtitle}</h3>
+              <p>${options.description}</p>
+            `)
+            .addTo(this.mapboxMap);
+        }
+
+        const markerClickListener = document.createElement('div');
+        markerClickListener.classList.add('visitation-map-app__marker');
+
+        const marker = new mapboxgl.Marker(markerClickListener)
           .setLngLat(options.coordinates)
-          .setPopup(popup)
+          .setPopup(popup) // can be set to null
           .addTo(this.mapboxMap);
 
         this.mapboxMap.on('zoomend', () => {
           const zoomLevel = this.mapboxMap.getZoom()
           const isOutOfRange = zoomLevel < options.zoomRange[0] || zoomLevel > options.zoomRange[1]
-          console.log(zoomLevel)
 
           if (isOutOfRange) {
             marker.remove();
           } else {
             marker.addTo(this.mapboxMap)
           }
-
         })
       },
 
@@ -122,42 +143,72 @@
         }
 
         this.mapboxMap.flyTo({
-          center: [-155.040, 19.650],
+          center: this.DEFAULT_COORDS,
           pitch: 30,
-          // speed: 0.5,
-          speed: 5,
-          zoom: 11.75,
+          speed: 0.5,
+          fadeDuration: 200,
+          zoom: this.DEFAULT_ZOOM,
         })
 
-        this.addPlace({
-          title: 'Nāwahīokalaniʻōpuʻu',
-          subtitle: 'Hawaiian medium education',
-          description: '16-120 ʻŌpūkahaʻia Street, Keaʻau',
-          logo: '/assets/logo-nawahi.svg',
+        this.addMarker({
+          id: 'hilo',
+          title: 'Hilo',
+          coordinates: [-155.0608837, 19.668792],
+          icon: { image: 'default-marker', size: 0.65 },
+          zoomTo: 11,
+          zoomToCoordinates: this.DEFAULT_COORDS,
+          zoomRange: [0, 9],
+        })
+
+        this.addMarker({
+          id: 'split-hilo',
+          title: 'Hilo Locations',
+          coordinates: [-155.0940677, 19.707875],
+          icon: { image: 'default-marker', size: 0.5 },
+          zoomTo: this.DEFAULT_ZOOM,
+          zoomToCoordinates: [-155.0931949, 19.6637184],
+          zoomRange: [9, 11.5],
+        })
+
+        this.addMarker({
+          id: 'split-keaau',
+          title: 'Keaʻau',
+          coordinates: [-155.0312562, 19.6056909],
+          icon: { image: 'default-marker', size: 0.5 },
+          zoomTo: this.DEFAULT_ZOOM,
+          zoomToCoordinates: [-155.0383607, 19.632712],
+          zoomRange: [9, 11.5],
+        })
+
+        this.addMarker({
           id: '16-120-opukahaia',
+          title: 'ʻAha Pūnana Leo o Hilo & Nāwahīokalaniʻōpuʻu',
+          subtitle: 'Hawaiian medium education schools',
+          description: '16-120 ʻŌpūkahaʻia Street, Keaʻau',
+          logo: ['/assets/logo-punanaleo.svg', '/assets/logo-nawahi.svg'],
           coordinates: [-155.0312562, 19.6056909],
           icon: { image: 'default-marker', size: 0.4 },
-          zoomRange: [9, 24],
+          zoomRange: [11.5, 24],
         })
 
         // TODO add punanaleo
 
-        this.addPlace({
+        this.addMarker({
+          id: '600-imiloa',
           title: 'ʻImiloa Astronomy Center',
           subtitle: 'Exhibition and planetarium',
           description: '600 ʻImiloa Place, Hilo',
           logo: '/assets/logo-imiloa.svg',
-          id: '600-imiloa',
           coordinates: [-155.090866, 19.7011223],
           zoomRange: [11.5, 24],
         })
 
-        this.addPlace({
+        this.addMarker({
+          id: '133-nowelo',
           title: 'Haleʻōlelo, Ka Haka ʻUla o Keʻelikōlani Hawaiian Language College',
           subtitle: 'Hawaiian Language College',
           description: '133 Nowelo Street, Hilo',
           logo: '/assets/logo-kahaka.svg',
-          id: '133-nowelo',
           coordinates: [-155.0848757, 19.7003206],
           zoomRange: [11.5, 24],
         })
@@ -179,6 +230,7 @@
 
   .visitation-map-app {
     transition: 300ms ease-out opacity;
+
     &--not-loaded {
       opacity: 0;
     }
@@ -207,21 +259,29 @@
       }
       .mapboxgl-popup-content {
         background: #fff;
-        background-image: linear-gradient(to bottom, var(--c-teal), #fff);
+        background-image: url('/assets/bg-popup.png');
         background-size: 100% 50px;
         background-repeat: no-repeat;
         background-position: top center;
         border-radius: var(--d-border-radius);
         padding: var(--d-space-small) var(--d-padding-small) var(--d-padding-small);
-        line-height: 120%;
         text-align: center;
         box-shadow: var(--d-box-shadow-large);
+        header {
+          margin: 0 0 var(--d-space-small);
+        }
         figure {
           display: inline-block;
-          margin: 0 auto var(--d-space-small);
+          margin: 0 var(--d-space) 0 0;
           background-image: url('/assets/logo-base.png');
           background-size: cover;
           border-radius: var(--d-border-radius-small);
+          border: 1px solid var(--c-teal--very-light);
+
+          &:last-of-type {
+            margin-right: 0;
+          }
+
           img {
             display: block;
           }
@@ -233,15 +293,18 @@
           font-size: 14px;
           font-weight: 600;
           margin-bottom: var(--d-space-small);
+          line-height: 120%;
         }
         h3 {
           color: var(--c-medium-gray);
           font-size: 12px;
           font-weight: 400;
+          line-height: 100%;
         }
         p {
           color: var(--c-medium-gray);
           font-size: 11px;
+          line-height: 100%;
           padding-top: var(--d-space-small);
           margin-top: var(--d-space-small);
           border-top: 1px solid var(--c-very-light-gray);
