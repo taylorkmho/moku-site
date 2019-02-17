313e0df65a7a54d1aaf16fbd107a7626a3d3ae75
taylorkmho <taylorkmho@gmail.com>
2019-02-16 22:16:54 -0800

    killing unused ResourcesSection

--- scripts/site.js
+++ scripts/site.js
@@ -3,7 +3,6 @@ import { modalSetUp } from './modal-set-up'
 import { mapSetUp } from './map-set-up'
 import { inlineVideoSetUp } from './inline-video-set-up'
 import { newsletterBalloonSetUp } from './newsletter-balloon-set-up'
-import { resourcesSectionSetUp } from './resources-section-set-up'
 import { resourcesFolderSetUp } from './resources-folder-set-up'
 import { smartThumbnailSetUp } from './smart-thumbnail-set-up'
 import { asideLinksSetUp } from './aside-links-set-up'
@@ -13,7 +12,6 @@ new modalSetUp()
 new mapSetUp()
 new inlineVideoSetUp()
 new newsletterBalloonSetUp()
-new resourcesSectionSetUp()
 new resourcesFolderSetUp()
 new smartThumbnailSetUp()
 new asideLinksSetUp()
